6440bc5576dfd4c8ebe80959cd0edaf863d56d67
Nora <noraej@live.no>
2021-12-25 22:35:01 +0100

    initial commit

---
+++ app/src/main/java/com/example/trafikkskilt/models/DrivingView.kt
@@ -0,0 +1,13 @@
+package com.example.trafikkskilt.models
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun DrivingView(navController: NavController){
+    Column() {
+        Text(text = )
+    }
+}
