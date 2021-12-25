89f37c7b211e17c21d62a31dc9e5d5eb0930513c
Nora <noraej@live.no>
2021-12-25 17:14:11 +0100

    added hamburgermenu to header and navigation

--- app/src/main/java/com/example/trafikkskilt/components/Header.kt
+++ app/src/main/java/com/example/trafikkskilt/components/Header.kt
@@ -1,15 +1,15 @@
 package com.example.trafikkskilt.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -23,6 +23,7 @@ import com.example.trafikkskilt.constants.headerHeigth
 import com.example.trafikkskilt.constants.headerTextSize
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.filled.Menu
+import androidx.navigation.NavController
 import com.example.trafikkskilt.constants.menuSize
 
 
@@ -32,7 +33,12 @@ class Header:ViewModel() {
 }
 
 @Composable
-fun HeaderComponent() {
+fun HeaderComponent(navController: NavController) {
+    var expanded by remember { mutableStateOf(false) }
+    val items = listOf("Start drive", "Calibrate camera")
+    var selectedIndex by remember { mutableStateOf(0) }
+
+
     Surface(
         color = colorResource(id = R.color.white).copy(alpha = 0.8f),//Color.Red.copy(alpha = 0.6f),
         modifier = Modifier.fillMaxSize().height(headerHeigth),
@@ -49,7 +55,23 @@ fun HeaderComponent() {
                 fontSize = headerTextSize,
 
             )
-            Menu()
+            //TODO: Clean up code here
+            HamburgerDropDownMenu(
+                items,
+                expanded,
+                selectedIndex,
+                updateMenuExpandStatus = {
+                    expanded = true
+                },
+                onDismissMenuView = {
+                    expanded = false
+                },
+                onMenuItemclick = { index->
+                    selectedIndex = index
+                    expanded = false
+                },
+                navController
+            )
         }
     }
 }
@@ -71,14 +93,49 @@ fun Logo() {
 }
 
 @Composable
-fun Menu(){
-    //var showMenu by remember { mutableStateOf(false) }
+fun HamburgerMenuButton(updateMenuExpandStatus : () -> Unit){
     IconButton(
         onClick = {
-            //TODO
-        },
+            updateMenuExpandStatus()        },
         modifier = Modifier.size(menuSize)
     ) {
         Icon(Icons.Filled.Menu,"")
+    }
+}
+
+
+//TODO: Make clicks on menu items take you the desired route
+@Composable
+fun HamburgerDropDownMenu(
+    menuItems: List<String>,
+    menuExpandedState: Boolean,
+    seletedIndex : Int,
+    updateMenuExpandStatus : () -> Unit,
+    onDismissMenuView : () -> Unit,
+    onMenuItemclick : (Int) -> Unit,
+    navController: NavController
+){
+    //The hamburger menu
+    HamburgerMenuButton(updateMenuExpandStatus)
+
+    //The dropdown menu that opens when you click the hamburger menu icon
+    DropdownMenu(
+        expanded = menuExpandedState,
+        onDismissRequest = { onDismissMenuView() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.surface)
+    ) {
+        menuItems.forEachIndexed { index, title ->
+            DropdownMenuItem(
+                onClick = {
+                    onMenuItemclick(index)
+                    if(seletedIndex == 0){
+                        navController.navigate("test")
+                    }
+                }) {
+                Text(text = title)
+            }
+        }
     }
 }
