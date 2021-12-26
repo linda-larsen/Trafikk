1477e9a6e1d440777003930ece0bd9bcc75194d1
Nora <noraej@live.no>
2021-12-26 18:44:00 +0100

    Made layout for driving view

--- app/src/main/java/com/example/trafikkskilt/models/DrivingView.kt
+++ app/src/main/java/com/example/trafikkskilt/models/DrivingView.kt
@@ -1,44 +1,82 @@
 package com.example.trafikkskilt.models
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.Icon
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.trafikkskilt.R
+import com.example.trafikkskilt.constants.paddingSize
+import com.example.trafikkskilt.constants.textSize
 
 @Composable
 fun DrivingView(navController: NavController){
-    Column() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+    ) {
         Text(
             text = stringResource(id = R.string.speedlimit_is),
             Modifier
-                .padding(30.dp)
+                .padding(paddingSize * 2)
                 .fillMaxHeight()
                 .weight(1f),
             fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
         )
 
-
-
-        Box {
-            Icon(
-                painter = painterResource(R.drawable.sign),
+        Box (
+            contentAlignment = Alignment.TopCenter,
+            modifier = Modifier.fillMaxHeight().weight(2f),
+        ) {
+            Image(
+                painterResource(R.drawable.emptysign),
                 contentDescription = stringResource(id = R.string.app_intro_image_content_description),
-                tint = colorResource(id = R.color.black),
                 modifier = Modifier
-                    .size(100.dp)
+                    .size(200.dp)
+                    .padding(paddingSize)
+                    .fillMaxHeight(),
             )
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Hello")
-            }
+            Text(
+                text = speedLimit(),
+                fontSize = 100.sp,
+                modifier = Modifier.padding(0.dp, 30.dp, 0.dp, 0.dp)
+
+            )
         }
+
+        Button(
+            onClick = {
+            //TODO: Probably something more
+            navController.navigate("startView")
+            },
+            modifier = Modifier
+                .padding(paddingSize*3)
+                .height(60.dp)
+                .width(200.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.button_color)),
+
+        ) {
+            Text(
+                text = stringResource(id = R.string.done),
+                fontSize = textSize
+                )
+        }
+
     }
+}
+
+fun speedLimit():String{
+    //TODO: This has to render according to speed limit
+    return "50"
 }
